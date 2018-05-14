cc5c66447f3b618d549a140358fd6ea80a609d3f
kamenjan <rok.arih@gmail.com>
2018-05-14 17:54:21 +0200

    implement gsap trim path simulation

--- Main.js
+++ Main.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
+import { TimelineLite } from 'gsap';
+
 import "./global.scss";
+import SvgImage from "./test.svg";
+
 
 import ParallaxElement from "./components/ParallaxElement/ParallaxElement";
 
@@ -8,22 +12,56 @@ export default class Main extends Component {
 
 	constructor() {
 		super();
-		this.state = {};
+		this.state = {
+			svgAnimationStyle: {}
+		};
+
+		this.mainReference = React.createRef();
+		this.tl = new TimelineLite();
 	}
 
 	/* Triggered before component loads */
 	componentWillMount() {}
 
 	/* Triggered when component loads */
-	componentDidMount() {}
+	componentDidMount() {
+		window.addEventListener("scroll", this.updatePosition);
 
-	componentWillUnmount() {}
+		this.path = this.mainReference.current.querySelector("path");
+		this.tl.to(this.path, 10, {strokeDashoffset:0});
+		this.tl.pause();
+
+		let style = {
+			strokeDasharray: 523,
+			strokeDashoffset: 523
+		};
+
+		this.setState({
+			svgAnimationStyle: style,
+			positionFromTop: 0
+		});
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener("scroll", this.updatePosition);
+	}
+
+	updatePosition = () => {
+		let offset = document.documentElement.scrollTop;
+		this.setState({ positionFromTop: offset });
+		if (offset > 535) return;
+		let position = offset / 535;
+		this.tl.progress(position);
+	};
 
 	render() {
 		return (
-			<div id={"app-container"}>
-				<ParallaxElement />
-				<div className={"generic-container"}></div>
+			<div id={"app-container"} ref={this.mainReference}>
+				{/*<ParallaxElement height={1038} />*/}
+				{/*This should be a container view. Using one container for SPA and multiple with react router for structured application*/}
+				<div className={"generic-container"}>
+					<SvgImage id={"test-svg"} style={this.state.svgAnimationStyle}/>
+				</div>
 			</div>
 		);
 	}
--- components/ParallaxElement/ParallaxElement.js
+++ components/ParallaxElement/ParallaxElement.js
@@ -19,7 +19,8 @@ export default class ParallaxElement extends Component {
 			positionFromTop: 0
 		};
 
-		this.bgLayer = React.createRef();
+		this.height = this.props.height;
+		console.log(this.height);
 	}
 
 	/* Triggered before component loads */
@@ -51,15 +52,15 @@ export default class ParallaxElement extends Component {
 		let color = this.animateBackgroundColor();
 
 		let layers = [
-			{name: "background", initialPosition: 0, modifier: 1.0, viewportTopOffset: this.state.positionFromTop, img: "", color: color},
-			{name: "soncek", initialPosition: 0, modifier: 1.2, viewportTopOffset: this.state.positionFromTop, img: imageParallaxSoncek},
-			{name: "farmidlands", initialPosition: 0, modifier: 0.5, viewportTopOffset: this.state.positionFromTop, img: imageParallaxFarmidlands},
-			{name: "midlands", initialPosition: 0, modifier: 0.3, viewportTopOffset: this.state.positionFromTop, img: imageParallaxMidlands},
-			{name: "foreground", initialPosition: 0, modifier: 0.1, viewportTopOffset: this.state.positionFromTop, img: imageParallaxForeground}
+			{name: "background", 	initialPosition: 0, modifier: 1.0, viewportTopOffset: this.state.positionFromTop, img: "", color: color},
+			{name: "soncek", 		initialPosition: 0, modifier: 1.2, viewportTopOffset: this.state.positionFromTop, img: imageParallaxSoncek},
+			{name: "farmidlands", 	initialPosition: 0, modifier: 0.5, viewportTopOffset: this.state.positionFromTop, img: imageParallaxFarmidlands},
+			{name: "midlands", 		initialPosition: 0, modifier: 0.3, viewportTopOffset: this.state.positionFromTop, img: imageParallaxMidlands},
+			{name: "foreground", 	initialPosition: 0, modifier: 0.1, viewportTopOffset: this.state.positionFromTop, img: imageParallaxForeground}
 		];
 
 		return (
-			<div id={"parallax-container"}>
+			<div id={"parallax-container"} style={{ height: this.props.height}}>
 				{layers.map(function(layerProps, index){
 					return <ParallaxLayer {...layerProps} key={index} />;
 				})}
